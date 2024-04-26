b2da4a6332918872bf79db1ddad9e867f36c8acf
Paweł Czekaj <paw.czekaj@gmail.com>
2024-04-26 23:49:58 +0200

    404 @mediaq

--- src/app/[...not_found]/page.tsx
+++ src/app/[...not_found]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NotFound() {
     return (
-        <div className = 'h-96 w-full flex justify-center items-center text-foreground font-bold text-3xl mt-48'>
+        <div className = 'h-96 w-full flex justify-center items-center text-foreground font-bold text-3xl mt-24 md:mt-48'>
             404 Page not Found
         </div>
     )
