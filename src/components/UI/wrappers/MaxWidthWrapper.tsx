5ff05d9ab603a2d4a9997d3d9066dca3a15676b0
Paweł Czekaj <paw.czekaj@gmail.com>
2024-04-26 18:20:04 +0200

    Initiated Next.js app

---
+++ src/components/UI/wrappers/MaxWidthWrapper.tsx
@@ -0,0 +1,10 @@
+import React, {ReactNode} from 'react';
+import {cn} from "@/lib/utils";
+
+export default function MaxWidthWrapper({className, children}: { className?: string, children: ReactNode }) {
+    return (
+        <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-10', className)}>
+            {children}
+        </div>
+    )
+}
