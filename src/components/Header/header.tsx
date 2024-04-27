a6a0c50ea7bec051b87e17c3cee13dcf76d6d652
Paweł Czekaj <paw.czekaj@gmail.com>
2024-04-27 23:19:06 +0200

    chevron animation; hover effects

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import MaxWidthWrapper from "@/components/UI/wrappers/MaxWidthWrapper";
 import Navigation from "@/components/Header/navigation";
+import Link from "next/link";
 
 
 export default function Header() {
     return (
         <header className='h-28 w-full flex flex-row justify-center border-b-2 border-b-muted-foreground shadow-lg'>
             <MaxWidthWrapper className='flex flex-row justify-between items-center'>
-                <div className={''}>
+                <Link href = {'/'} className={'hover:opacity-70'}>
                     <h4 className={''}><span className={'text-primary'}>Pan </span> Paweł</h4>
                     <h5>Blog Osobisty</h5>
-                </div>
+                </Link>
                 <img src='images/me_glasses_3.png' className={'h-full w-auto'}/>
                 <Navigation/>
             </MaxWidthWrapper>
--- src/components/Header/navigation.tsx
+++ src/components/Header/navigation.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+"use client"
+
+import React, {useState} from 'react';
 import Link from "next/link";
 import {ChevronDown} from "lucide-react";
+import {cn} from "@/lib/utils";
 
 
 
 export default function Navigation() {
+    const [dropActive, setDropActive] = useState<boolean>(false)
     return (
         <ul className = 'flex flex-row font-medium text-foreground' >
-            <Link href={'/'} className={'flex items-center mr-12'}>Artykuły<ChevronDown size = {'18'}/></Link>
-            <Link href={'/about'}>O mnie</Link>
+            <Link href={'/'} className={'flex items-center mr-12 hover:opacity-70'}
+                  onClick={()=>{setDropActive(!dropActive)}}
+            >
+                Artykuły
+                <ChevronDown size = {'18'} className={cn('transition-all', dropActive && '-rotate-180')}/>
+            </Link>
+            <Link href={'/about'} className={'hover:opacity-70'}>O mnie</Link>
         </ul>
     )
 }
