f37b8821df449844940bb400237caa9e19033fed
Paweł Czekaj <paw.czekaj@gmail.com>
2024-05-07 20:55:59 +0200

    adjusted accent color for ghostvariant

--- src/components/UI/Articles/categories.tsx
+++ src/components/UI/Articles/categories.tsx
@@ -5,19 +5,20 @@ import {cn} from "@/lib/utils";
 import {buttonVariants} from "@/components/UI/button";
 
 
-export default async function Categories({className}:{className?:string}) {
+export default async function Categories({className}: { className?: string }) {
 
     const {getCategories} = useCategoriesController()
     const categories = await getCategories();
 
     return (
-        <div className = {`flex flex-row py-3 border-b-gray-300 border-b-2 ${className}`}>
-            <div className = 'font-normal text-xl md:text-2xl w-auto flex items-center'>
+        <div className={`flex flex-row py-3 border-b-gray-300 border-b-2 ${className}`}>
+            <div className='font-normal text-xl md:text-2xl w-auto flex items-center'>
                 Kategorie:
             </div>
-            <div className = 'flex flex-row flex-wrap items-center'>
-                {categories && categories.map((cat, i):ReactNode=>{
-                    return <Link href = '/articles/[slug]' as = {`/articles/${cat.id}`} className = {cn(buttonVariants({variant: 'ghost'}), 'uppercase flex justify-center items-center leading-3 text-xs md:text-base mx-2 lg:mx-4 my-0 font-normal hover:opacity-70')}>
+            <div className='flex flex-row flex-wrap items-center'>
+                {categories && categories.map((cat, i): ReactNode => {
+                    return <Link href='/articles/[slug]' as={`/articles/${cat.id}`}
+                                 className={cn(buttonVariants({variant: 'ghost'}), 'uppercase flex justify-center items-center leading-3 text-xs md:text-base mx-2 lg:mx-4 my-0 font-normal')}>
                         {cat.name}
                     </Link>
                 })}
