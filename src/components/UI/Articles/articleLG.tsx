66b1711701618eb2ad93c9ede51b4204c09e6f06
Paweł Czekaj <paw.czekaj@gmail.com>
2024-05-10 22:39:19 +0200

    fixed image roundness

--- src/components/UI/Articles/articleLG.tsx
+++ src/components/UI/Articles/articleLG.tsx
@@ -10,7 +10,7 @@ export default function ArticleLG({article, currentCatId}:{article:Article, curr
     return (
         <div className = 'ArticleLG'>
             <TagStrip categories={article.categories} currentCatId={currentCatId || null} tags = {article.tags}/>
-            <Image src={`${process.env.BASE_URL}${article.image_link}`} alt={`${article.title}`} width={642} height={428}/>
+            <Image className={'rounded-3xl'} src={`${process.env.BASE_URL}${article.image_link}`} alt={`${article.title}`} width={642} height={428}/>
             <h1>{article.title}</h1>
             <div className = ''>{article.createdAt}</div>
             {<BlocksRenderer content={article.body}/>}
